ba22e09810c7eaaa8fd784e5c1297e2253114b70
Romanz16 <zhyshkovych16@gmail.com>
2019-09-24 14:58:26 +0300

    header Menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,13 +7,35 @@ import { Location } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
+  ulMenu: string;
+  ulMenuHeight: string;
+  checkMenu: boolean = true;
   constructor(private route: ActivatedRoute,
     private location: Location) {
-    
+
   }
 
   ngOnInit() {
   }
- 
+  public menu(): void {
+    if (this.checkMenu) {
+      this.ulMenuHeight = '205px';
+      this.ulMenu = 'flex';
+      this.checkMenu = false;
+    } else {
+      this.ulMenuHeight = '41px';
+      this.checkMenu = true;
+      this.ulMenu = 'none';
+    }
+  }
+  onResize(event) {
+    if (event.target.innerWidth > 400) {
+      this.ulMenuHeight = '41px';
+      this.ulMenu = 'flex';
+    } else{
+      this.ulMenu = 'none';
+    }
+  }
 }
